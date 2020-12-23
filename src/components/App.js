700a38e37bd1c4579a55e2520c3f47b5433f7891
wilber <parionahuaraca@gmail.com>
2020-12-23 01:44:46 -0500

    Se instala node-sass, tailwindcss, postcss, postcss-cli, autoprefixer  y se importa style.scss

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
- 
+
 const  App = ()=> {
   return (
     <div className="App">
-        <h1>Plataforma de educacion online </h1>   
+        <h1  className="text-3xl font-semibold text-center mb-7 mt-7" >Plataforma de educacion online </h1>   
+
+
+        <div className="container m-auto"> 
+           <div className="grid  grid-cols-3 gap-5 " >
+              <div className="py-8 text-center bg-blue-600  rounded-md" > 
+                  <h1 className="text-lg text-white ">Title 1</h1>
+              </div>
+              <div className="py-8 text-center bg-blue-600  rounded-md" > 
+                  <h1 className="text-lg text-white ">Title 1</h1>
+              </div>
+              <div className="py-8 text-center bg-blue-600  rounded-md" > 
+                  <h1 className="text-lg text-white ">Title 1</h1>
+              </div>
+           </div>
+         </div>
     </div>      
   );
-      
+
 }
 
 export default App;
